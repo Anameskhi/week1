5e5ceb8b7c248c924cdaf8d4ab6bca60d4dd134d
Anameskhi <ameskhi@unisens.ge>
2022-03-30 15:11:01 +0400

    change task7

--- task_7.rb
+++ task_7.rb
@@ -1,16 +1,18 @@
+HANDRED = 100
 class Fibonacci
-  @first_num  = 0
-  @second_num = 1
-  HANDRED = 100
-  def self.fibonacci_number
-    empty_arr= []
-   HANDRED.times do
-    empty_arr << @first_num
-    @first_num, @second_num = @second_num, @first_num + @second_num
-   end
-   empty_arr
+  def initialize
+    @first_num  = 0
+    @second_num = 1
+  end
+
+  def get_fibonacci_number
+    fibonacci_nums = []
+    HANDRED.times do
+      fibonacci_nums << @first_num
+      @first_num, @second_num = @second_num, @first_num + @second_num
+    end
+    fibonacci_nums
   end
 end
 
-
-p Fibonacci.fibonacci_number
+p Fibonacci.new.get_fibonacci_number
