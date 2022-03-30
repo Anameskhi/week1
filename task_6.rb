7859647f4e07e9e6dc64dbb809bea1489cf2a9c1
Anameskhi <ameskhi@unisens.ge>
2022-03-30 14:55:40 +0400

    change task6

--- task_6.rb
+++ task_6.rb
@@ -1,10 +1,13 @@
+HUNDRED = 100
+TEN = 10
+FIVE = 5
+
 class Step
-  HUNDRED = 100
-  TEN = 10
-  FIVE = 5
-  def self.multiply_by_five
+  def initialize; end
+
+  def multiply_by_five
     (TEN..HUNDRED).step(FIVE).to_a
   end
 end
 
-p Step.multiply_by_five
+p Step.new.multiply_by_five
