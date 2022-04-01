959e0765c7e5f8195f2e8dff68064302662ec95a
Anameskhi <ameskhi@unisens.ge>
2022-04-01 12:14:26 +0400

    add validation

--- task_9.rb
+++ task_9.rb
@@ -2,13 +2,23 @@ require 'date'
 
 class CountDays
   def initialize(year, month, day)
-    @year = year.to_f
-    @month = month.to_f
-    @day = day.to_f
+    @year = year
+    @month = month
+    @day = day
+    validation_dates(@year, @month, @day)
   end
 
   def count_days
     Date.new(@year, @month, @day).yday
+  end
+
+  private
+
+  def validation_dates(year, month, day)
+    if Date.valid_date?(year.to_i, month.to_i, day.to_i) == false
+      puts "#{year}-#{month}-#{day} date doesn't exist "
+      exit 1
+    end
   end
 end
 
