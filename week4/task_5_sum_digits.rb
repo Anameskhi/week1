905b0c05d48acb7d95f97cc4f33067bd01ccfe55
Ameskhi <ameskhi@unisens.ge>
2022-04-06 22:57:59 +0400

    make changes

--- week4/task_5_sum_digits.rb
+++ week4/task_5_sum_digits.rb
@@ -5,16 +5,29 @@ class SumDigits
   end
 
   def call 
+    validate_num
     sum_of_digits
+  rescue StandardError => e
+    warn e
+  
   end
 
   def sum_of_digits
-    (@num1..@num2).to_a.sum
+    (@num1.to_i..@num2.to_i).to_a.sum
+  end
+  
+  def validate_num
+    raise ArgumentError, "your numbers isn't integers, please try again" unless @num1.to_i.to_s == @num1 || @num2.to_i.to_s == @num2
   end
 
 
 
 end
 
+puts "tell me first num"
+num1 = gets.chomp
+puts "tell me second num"
+num2 = gets.chomp
 
-p SumDigits.new(1,10).call
+
+p SumDigits.new(num1, num2).call
