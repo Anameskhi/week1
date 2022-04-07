65a89a9409f439ec043b456277d3ae3ae3822bdf
Anameskhi <ameskhi@unisens.ge>
2022-04-07 09:24:36 +0400

    sum of digits task

--- week4/task_5_sum_digits.rb
+++ week4/task_5_sum_digits.rb
@@ -1,33 +1,29 @@
 class SumDigits
-  def initialize(num1,num2)
-   @num1, @num2 = [num1, num2].sort
-
+  def initialize(num1, num2)
+    @num1, @num2 = [num1, num2].sort
   end
 
-  def call 
+  def call
     validate_num
     sum_of_digits
   rescue StandardError => e
     warn e
-  
   end
 
   def sum_of_digits
     (@num1.to_i..@num2.to_i).to_a.sum
   end
-  
+
   def validate_num
-    raise ArgumentError, "your numbers isn't integers, please try again" unless @num1.to_i.to_s == @num1 || @num2.to_i.to_s == @num2
+    return if @num1.to_i.to_s == @num1 && @num2.to_i.to_s == @num2
+
+    raise ArgumentError, "your numbers isn't integers, please try again"
   end
-
-
-
 end
 
-puts "tell me first num"
+puts 'tell me first num'
 num1 = gets.chomp
-puts "tell me second num"
+puts 'tell me second num'
 num2 = gets.chomp
 
-
-p SumDigits.new(num1, num2).call
+puts SumDigits.new(num1, num2).call
