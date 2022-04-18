0292fee4f0a2f115027d792eed4b849464acd1a6
Anameskhi <ameskhi@unisens.ge>
2022-04-18 16:46:11 +0400

    solved

--- week5/stockpicker/stockpicker.rb
+++ week5/stockpicker/stockpicker.rb
@@ -8,28 +8,29 @@ class StockPicker
     validate_array_of_stock
     gets_pair
   rescue ArgumentError => e 
-    warn e
+    e.message
   end
 
   def gets_pair
    buy_day = 0
     sell_day = 0
     max_diff = 0
+    @pair = []
     @array.each_with_index do |num, index|
       i = 0
       while (index + i < @array_size)
         if ((@array[index+i] - @array[index]) > max_diff)
           max_diff = @array[index+i] - @array[index]
-          buy_day = @array[index+i]
-          sell_day = @array[index]
+          buy_day_index = index+i
+          sell_day_index = index
+          @pair = [sell_day_index,buy_day_index]
         end
       i += 1
       end
-
     end
-    pair = [buy_day,sell_day]
-    return "the pair is #{pair} and the profit is #{max_diff} "
-end
+    @pair
+  end
+    
 
   def is_integer?
     @array.all?{|num| num.is_a?(Integer)}
@@ -39,4 +40,4 @@ end
   raise ArgumentError, "Invalid Input" unless is_integer? && @array.is_a?(Array)
   end
 end
-puts StockPicker.new([17,6,9,14,8,6,7,15]).call
+
