83f586240491159a921756b43bf555ccff6879ac
Anameskhi <ameskhi@unisens.ge>
2022-04-20 12:02:21 +0400

    solved

---
+++ week6_7/task_1/task_1_bubble_sort.rb
@@ -0,0 +1,39 @@
+class BubbleSort
+  def initialize(array)
+    @array = array
+  end
+
+  def call
+    validate_array
+    bubble_sort_array
+  rescue ArgumentError => e
+    e.message
+  end
+
+  private
+
+  def bubble_sort_array
+    return @array if @array.length <= 1
+
+    @array.each do
+      swap = false
+      @array.each_with_index do |num, index|
+        break if index == @array.length - 1
+
+        if num > @array[index + 1]
+          @array[index], @array[index + 1] = @array[index + 1], @array[index]
+          swap = true
+        end
+      end
+      break unless swap
+    end
+    @array
+  end
+
+  def validate_array
+    raise ArgumentError, 'Invalid Input' unless @array.all? { |elem| elem.is_a?(Integer) }
+  end
+end
+
+
+
