1c2b10c768fa663721773158fb6f59a32dc9e5b0
Anameskhi <ameskhi@unisens.ge>
2022-04-28 12:53:23 +0400

    last changes

--- week6_7/task_2/task_2_tic_tac_toe.rb
+++ week6_7/task_2/task_2_tic_tac_toe.rb
@@ -1,28 +1,26 @@
- 
-    WIN_COMBINATIONS = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [6, 4, 2],
-      [0, 4, 8]
-    ]
-    
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [6, 4, 2],
+  [0, 4, 8]
+].freeze
+
 class TicTacToe
-  
   def initialize
     @current_index = 0
 
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts " ----------- "
+    puts ' ----------- '
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts " ----------- "
+    puts ' ----------- '
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
@@ -31,64 +29,63 @@ class TicTacToe
   end
 
   def move(input_to_index, player)
-      @board[input_to_index] = player
+    @board[input_to_index] = player
   end
 
   def position_taken?(input_to_index)
-    if ["", " ", nil].any? @board[input_to_index]
-      return false 
+    if ['', ' ', nil].any? @board[input_to_index]
+      false
     else
-      return true
+      true
     end
   end
 
   def valid_move(input_to_index)
-      !position_taken?(input_to_index) && input_to_index.between?(0, 8)
+    !position_taken?(input_to_index) && input_to_index.between?(0, 8)
   end
 
   def turn_count
-   count = 0
-      @board.each do |i|
-          count += 1 if i == "X" || i == "O"
-      end
-   count
+    count = 0
+    @board.each do |i|
+      count += 1 if %w[X O].include?(i)
+    end
+    count
   end
 
   def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+    turn_count.even? ? 'X' : 'O'
   end
 
   def turn
-    if @current_index == 0 
-      puts "Player1"
-      valid_moves
-    else  
-      puts "Player2"
-      valid_moves
+    if @current_index.zero?
+      puts 'Player1'
+    else
+      puts 'Player2'
     end
+    valid_moves
   end
 
   def valid_moves
     choice = gets.chomp!
     position = input_to_index(choice)
-  if valid_move(position)
-    move(position,current_player)
-    display_board
-  else
-    turn
+    if valid_move(position)
+      move(position, current_player)
+      display_board
+    else
+      turn
+    end
   end
-end
 
   def won?
     WIN_COMBINATIONS.any? do |combo|
-    @board[combo[0]] == @board[combo[1]] &&
-    @board[combo[1]] == @board[combo[2]] && 
-    position_taken?(combo[0])
+      @board[combo[0]] == @board[combo[1]] &&
+        @board[combo[1]] == @board[combo[2]] &&
+        position_taken?(combo[0])
     end
   end
 
   def full?
-    @board.all?{|token| token == "X" || token == "O"}
+    @board.all? { |token| %w[X O].include?(token) }
   end
 
   def draw?
@@ -100,15 +97,15 @@ end
   end
 
   def winner_is
-    if @current_index == 0
-      "congretulation wiiner is player2"
-      else
-      "congretulation winner player1"
-      end
+    if @current_index.zero?
+      'congretulation wiiner is player2'
+    else
+      'congretulation winner player1'
+    end
   end
 
   def play
-    while !over?
+    until over?
       turn
       @current_index = (@current_index + 1) % 2
     end
@@ -121,4 +118,5 @@ end
 end
 
 p TicTacToe.new.play
+
 
