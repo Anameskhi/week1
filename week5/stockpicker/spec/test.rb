7f64621bbb5bce3e8f3555ffdd60c510e6a09c65
Anameskhi <ameskhi@unisens.ge>
2022-04-28 10:43:27 +0400

    make changes

--- week5/stockpicker/spec/test.rb
+++ week5/stockpicker/spec/test.rb
@@ -1,23 +1,22 @@
-require "../stockpicker"
-require "rspec/autorun"
+# frozen_string_literal: true
+
+require '../stockpicker'
+require 'rspec/autorun'
 
 describe StockPicker do
-    let(:stocks) { [17, 3, 6, 9, 15, 8, 6, 1, 10] }
-    let(:empty_array) { [] }
-    let(:invalid_array) { [2,35,7,3,nil,'2','sdf'] }
+  let(:stocks) { [17, 3, 6, 9, 15, 8, 6, 1, 10] }
+  let(:empty_array) { [] }
+  let(:invalid_array) { [2, 35, 7, 3, nil, '2', 'sdf'] }
 
-    it "should return index of array [4,1]" do
-      expect(StockPicker.new(stocks).call).to eq([1, 4])
-    end
-
-    it "araay should be empty" do
-      expect(StockPicker.new(empty_array).call).to eq([])
-    end
-
-    it "araay should be invalid" do
-      expect(StockPicker.new(invalid_array).call).to eq("Invalid Input")
-    end
+  it 'should return index of array [4,1]' do
+    expect(StockPicker.new(stocks).call).to eq([1, 4])
   end
 
+  it 'araay should be empty' do
+    expect(StockPicker.new(empty_array).call).to eq([])
+  end
 
-  
+  it 'araay should be invalid' do
+    expect(StockPicker.new(invalid_array).call).to eq('Invalid Input')
+  end
+end
--- week5/stockpicker/stockpicker.rb
+++ week5/stockpicker/stockpicker.rb
@@ -7,41 +7,45 @@ class StockPicker
   def call
     validate_array_of_stock
     gets_pair
-  rescue ArgumentError => e 
+  rescue ArgumentError => e
     e.message
   end
-  
+
   private
-  
+
+  def index_of_pairs(current_index, index)
+    if (@array[current_index] - @array[index]) > @max_diff
+      @max_diff = @array[current_index] - @array[index]
+      buy_day_index = current_index
+      sell_day_index = index
+      @pair = [sell_day_index, buy_day_index]
+    end
+  end
+
+  def required_pair(current_index, index)
+    while current_index < @array_size
+      index_of_pairs(current_index, index)
+      current_index += 1
+    end
+  end
+
   def gets_pair
-   buy_day = 0
-    sell_day = 0
-    max_diff = 0
+    @max_diff = 0
     @pair = []
-    @array.each_with_index do |num, index|
-      i = 0
-      while (index + i < @array_size)
-        if ((@array[index+i] - @array[index]) > max_diff)
-          max_diff = @array[index+i] - @array[index]
-          buy_day_index = index+i
-          sell_day_index = index
-          @pair = [sell_day_index,buy_day_index]
-        end
-      i += 1
-      end
+    @array.each_with_index do |_, index|
+      current_index = index
+      required_pair(current_index, index)
     end
     @pair
   end
-    
 
   def is_integer?
-    @array.all?{|num| num.is_a?(Integer)}
+    @array.all? { |num| num.is_a?(Integer) }
   end
 
   def validate_array_of_stock
-  raise ArgumentError, "Invalid Input" unless is_integer? && @array.is_a?(Array)
+    raise ArgumentError, 'Invalid Input' unless is_integer? && @array.is_a?(Array)
   end
 end
-
 
 
